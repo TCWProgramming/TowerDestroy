ac2d889b74ffba96bbee3b2f2acf17ddb4b29b8d
Anthony Ha <haa4@uci.edu>
2018-06-05 04:25:51 -0700

    Prototype projectile

---
+++ Assets/Scripts/LaunchedProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchedProjectile : MonoBehaviour {
+
+	private Collider[] hitObjects;
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag == "TowerPart"){
+			hitObjects = Physics.OverlapSphere(transform.position, 2.5f);
+			pushObjects(hitObjects);
+		}
+	}
+
+	void pushObjects(Collider[] colls){
+		foreach (Collider hit in colls){
+			Rigidbody rb = hit.GetComponent<Rigidbody>();
+			if (rb != null && rb.gameObject.tag == "TowerPart"){
+				rb.AddExplosionForce(2f, transform.position, 2.5f, 1f);
+			}
+		}
+	}
+}
