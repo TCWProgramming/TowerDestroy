51d8b8259655acd73504fb847afa6d61a64ce864
Ayelen Sol <asforni@uci.edu>
2018-06-08 15:11:30 -0700

    Test For Projectile
    I added a test scene that contains a tower and projectile. For the projectile I added a spawn point so when space is pressed the projectile will shoot towards the castle. This is to test shooting of the projectile and I also added the Launch Script to the projectile to see how the projectile hitting the tower would react. I did not see a way to test that otherwise since there was no way to shoot the projectile. Was not able to test however due to some networking issues. Not sure what the problem there is.

---
+++ Assets/TestProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestProjectile : MonoBehaviour {
+
+	public GameObject projectile;
+	public Transform projectileSpawn;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+			Fire ();
+	}
+
+	void Fire(){
+		var proj = (GameObject)Instantiate (
+			           projectile, projectileSpawn.position, projectileSpawn.rotation);
+
+		projectile.GetComponent<Rigidbody> ().velocity = projectile.transform.forward * 5;
+
+		Destroy (projectile, 2.0f);
+	}
+}
