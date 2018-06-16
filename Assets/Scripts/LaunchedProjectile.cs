914b5a1d05cba4670decd3e02691896f830b8962
Anthony Ha <haa4@uci.edu>
2018-06-16 02:02:29 -0700

    Projectile spawns particles (untextured) on collision

--- Assets/Scripts/LaunchedProjectile.cs
+++ Assets/Scripts/LaunchedProjectile.cs
@@ -6,12 +6,26 @@ using UnityEngine.Networking;
 public class LaunchedProjectile : MonoBehaviour {
 
 	private Collider[] hitObjects;
+    public float r = 2.5f;
+    public GameObject explodeEffect;
+    private GameObject explInstance;
 
 	void OnCollisionEnter(Collision coll){
-		hitObjects = Physics.OverlapSphere(transform.position, 2.5f);
+		hitObjects = Physics.OverlapSphere(transform.position, r);
+        explInstance = Instantiate(explodeEffect, transform.position, Quaternion.identity);
+
 		
 		foreach (Collider hit in hitObjects){
 			Destroy(hit.gameObject);
 		}
+
+        Invoke("StopParticles", 1.5f);
+        Destroy(this.gameObject);
+
 	}
+
+    void StopParticles()
+    {
+        Destroy(explInstance);
+    }
 }
